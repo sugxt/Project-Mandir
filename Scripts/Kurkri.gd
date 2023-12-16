f9fd37691238d49e2ec1d7118f07332ebfb38090
Sugat Poudyal <official.sugat@gmail.com>
2023-12-16 19:09:56 +0545

    Fixed Collision and Enemy Movement

--- Scripts/Kurkri.gd
+++ Scripts/Kurkri.gd
@@ -2,11 +2,13 @@ extends CharacterBody2D
 
 @export var health = 200
 @onready var player = get_node("/root/Main/Player")
+@onready var speed = 200
 
 func _physics_process(delta):
-	var speed: float = 0.03 # put wanted speed here
 	look_at(player.global_position)
-	self.position = lerp(self.position,player.global_position,speed)
+	var direction = (player.global_position - self.position).normalized()
+	self.position += direction * speed * delta
+	
 func onDamage(damage_number):
 	print("damage called: ",damage_number)
 	health = health - damage_number
@@ -18,7 +20,4 @@ func onDamage(damage_number):
 func _on_area_2d_body_entered(body):
 	if body.name == "Player":
 		body.on_damage(500)
-		if $Area2D.has_overlapping_bodies():
-			await get_tree().create_timer(1).timeout
-			body.on_damage(200) # Change it so the damage keeps happening instead of once
 		
